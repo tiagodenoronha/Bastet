464e973e737fdc03e44365b4ce60b0a27901094a
Tiago Noronha <tiago.noronha@unipartner.com>
2019-04-07 16:25:22 +0100

    Added sanitizer text and global flow.

---
+++ TextAnalyzer/Models/LUISRecognizedText.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalyzer.Models
+{
+    internal class LUISRecognizedText
+    {
+        internal LuisIntent Intent { get; set; }
+        internal IEnumerable<string> ExtractedEntities { get; set; }
+    }
+}
--- TextAnalyzer/TextAnalyzerFunction.cs
+++ TextAnalyzer/TextAnalyzerFunction.cs
@@ -1,14 +1,43 @@
+using System;
+using Microsoft.Azure.ServiceBus;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using TextAnalyzer.Models;
 
 namespace TextAnalyzer
 {
     public static class TextAnalyzerFunction
     {
         [FunctionName("TextAnalyzerFunction")]
-        public static void Run([ServiceBusTrigger("myqueue", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger("myqueue", Connection = "ServiceBusConnectionString")]string htmlText, ILogger log)
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            var method = "Main";
+            log.LogInformation(string.Format("{0} - {1}", method, "IN"));
+
+            var sanitizedText = HelperMethods.HtmlToPlainText(htmlText);
+
+            log.LogInformation(string.Format("{0} - {1}", method, "Extracting information from LUIS."));
+            var result = ExtractEntitiesFromLUIS(sanitizedText);
+
+            if (result.Intent == LuisIntent.FAQ)
+            {
+                log.LogInformation(string.Format("{0} - {1}", method, "Getting FAQs Answer."));
+                var response = CheckQnAMakerForResponse(sanitizedText);
+                InsertAnswerIntoCRM(response);
+            }
+            else
+            {
+                log.LogInformation(string.Format("{0} - {1}", method, "Getting Keywords and Sentiment"));
+                var response = ExtractKeywordsAndSentimentFromTextAnalytics(sanitizedText);
+                InsertKeywordAndSentimentoIntoCRM(response);
+            }
+
         }
+
+        private static void InsertKeywordAndSentimentoIntoCRM(object response) => throw new NotImplementedException();
+        private static void InsertAnswerIntoCRM(object response) => throw new NotImplementedException();
+        static object CheckQnAMakerForResponse(string textToAnalyze) => throw new NotImplementedException();
+        static object ExtractKeywordsAndSentimentFromTextAnalytics(string textToAnalyze) => throw new NotImplementedException();
+        static LUISRecognizedText ExtractEntitiesFromLUIS(string textToAnalyze) => throw new NotImplementedException();
     }
 }
